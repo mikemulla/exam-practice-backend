55d7f734cc343066e54bf11255ed551194d7fb6a
mikemulla <mikeadebambo@gmail.com>
2026-04-11 23:38:40 +0200

    restore detailed result review

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 /* CORS CONFIG */
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://bambo-app.vercel.app/"],
+    origin: ["http://localhost:5173", "https://bambo-app.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
